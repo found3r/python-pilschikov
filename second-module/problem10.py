81f924b9821b17884d885da1f20cb3f4cf14d800
Leonid Zhavoronkov <halfleonlife@gmail.com>
2021-03-15 02:26:05 +0300

    feat. Second module problem 10 statement 'k'

--- second-module/problem10.py
+++ second-module/problem10.py
@@ -27,3 +27,16 @@ elif (x > 0) or (y > 0) or (z > 0):
     print('Statement "е" is true')
 else:
     print('Statement "ж" is true')
+
+year = int(input('Input year = '))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print('It is a leap year')
+        else:
+            print('It is not a leap year')
+    else:
+        print('It is not a leap year')
+else:
+    print('It is not a leap year')
