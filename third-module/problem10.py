510f0683573f42751139a69003cb8553b9c00648
Leonid Zhavoronkov <halfleonlife@gmail.com>
2021-03-19 20:49:42 +0300

    feat. Third module problem 10

---
+++ third-module/problem10.py
@@ -0,0 +1,8 @@
+#  Copyright (c) 2021.
+#  Leonid Zhavoronkov (halfleonlife@gmail.com)
+
+
+a = int(input('Input number "a": '))
+print(f'{a}')
+print(f'{a ** 3} {a ** 6}')
+print(f'{a ** 6} {a ** 3} {a}')
