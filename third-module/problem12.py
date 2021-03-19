267fad78979210e2859d33b3d13fc7a0698e6576
Leonid Zhavoronkov <halfleonlife@gmail.com>
2021-03-19 21:52:35 +0300

    feat. Third module problem 12

---
+++ third-module/problem12.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2021.
+#  Leonid Zhavoronkov (halfleonlife@gmail.com)
+
+
+import math
+if math.exp(math.pi) > (math.pi ** math.exp(1)):
+    print('true')
+else:
+    print('false')
