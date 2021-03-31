b4cf89a8a2112951307def27b4da34b66aca6d08
Leonid Zhavoronkov <halfleonlife@gmail.com>
2021-03-31 23:43:25 +0300

    feat: Problem 9

---
+++ module_06/problem09.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2021.
+#  Leonid Zhavoronkov (halfleonlife@gmail.com)
+
+
+import string
+
+print('\nCapital A-Z alphabet: ')
+for letter in string.ascii_uppercase:
+    print(letter, end='')
