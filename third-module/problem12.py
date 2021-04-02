c4146143e105a537f4aa9d36080297f155095f04
found3r <halfleonlife@gmail.com>
2021-04-02 21:07:43 +0300

    refactor(branches): Reassambled module 3 to a separate branch

--- third-module/problem12.py
+++
@@ -1,9 +0,0 @@
-#  Copyright (c) 2021.
-#  Leonid Zhavoronkov (halfleonlife@gmail.com)
-
-
-import math
-if math.exp(math.pi) > (math.pi ** math.exp(1)):
-    print('true')
-else:
-    print('false')
