acbd17b99bce5793724da6ff1af4b69ba4b766ca
found3r <halfleonlife@gmail.com>
2021-04-09 07:45:41 +0300

    refactor(problem10): Reassabmle into array

--- module_09/problem10.py
+++ module_09/problem10.py
@@ -4,19 +4,21 @@
 
 import random
 
-
 array = []
 for n in range(4):
     row = []
-    for k in range(7):
-        # row.append(random.randint(-20, 20))
-        row.append(k)
+    for k in reversed(range(7)):
+        row.append(random.randint(-20, 20))
+        # row.append(k)
     array.append(row)
 
 print(array)
 
-for n in range(6):
-    if array[0][n] > array[0][n + 1]:
-        
+maximum_row = []
+for n in range(4):
+    maximum_row.append(max(array[n]))
 
-print(*array)
+maximum_column = max(maximum_row)
+maximum_column_index = maximum_row.index(maximum_column)
+
+print(array[maximum_column_index].index(maximum_column))
