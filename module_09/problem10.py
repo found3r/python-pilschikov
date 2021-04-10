38f844743cf10729c2ea07d012defa7fa4d8f359
found3r <halfleonlife@gmail.com>
2021-04-10 23:31:48 +0300

    feat(problem10): Module 8 problem 10 v1.0

--- module_09/problem10.py
+++ module_09/problem10.py
@@ -4,21 +4,43 @@
 
 import random
 
+
+rows_amount = 4
+columns_amount = 7
+row_index = int
+column_index = int
+
 array = []
-for n in range(4):
+for n in range(rows_amount):
     row = []
-    for k in reversed(range(7)):
+    for k in reversed(range(columns_amount)):
         row.append(random.randint(-20, 20))
-        # row.append(k)
     array.append(row)
 
 print(array)
 
-maximum_row = []
-for n in range(4):
-    maximum_row.append(max(array[n]))
+maximum_per_row = []
 
-maximum_column = max(maximum_row)
-maximum_column_index = maximum_row.index(maximum_column)
+for n in range(rows_amount):
+    maximum_per_row.append(max(array[n]))
 
-print(array[maximum_column_index].index(maximum_column))
+max_element = max(maximum_per_row)
+
+for n in range(rows_amount):
+    for k in range(columns_amount):
+        if array[n][k] == max_element:
+            row_index = n
+            column_index = k
+
+print(f'Max element = {max_element}')
+print(f'Row = {row_index + 1}')
+print(f'Column = {column_index + 1}')
+
+
+container = array[row_index]
+array.remove(container)
+container.sort()
+array.insert(0, container)
+
+for n in range(rows_amount):
+    print(*array[n])
